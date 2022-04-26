0c8ea90800c6a0b5d450415be10855c634c42a1c
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-04-26 18:46:30 +0200

    Update red_black_tree.py
    Creating class(Node) and class(RedBlackTree)

---
+++ red_black_tree.py
@@ -0,0 +1,18 @@
+# Implementation of Red-Black Tree in python
+
+
+class Node():
+    def __init__(self, word):
+        self.word = word
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.color = 1
+
+
+class RedBlackTree():
+    def __init__(self):
+        self.nill = Node(0)  # creation of the NILL node
+        self.nill.color = 0  # setting the NILL node's color as black
+        self.root = self.nill  # setting the RBTree's root as NILL as it is empty
+        # some attributes were not added here since they were added in node declaration
