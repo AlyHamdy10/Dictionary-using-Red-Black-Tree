170d5d79a778d4be279966dd08c69000331d8907
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-04-26 22:32:46 +0200

    Update red_black_tree.py
    Adding insert function.
    function to fix after inserting is still to be added.

--- red_black_tree.py
+++ red_black_tree.py
@@ -17,3 +17,39 @@ class RedBlackTree():
         self.root = self.nill  # setting the RBTree's root as NILL as it is empty
         self.count = 0
         # some attributes were not added here since they were added in node declaration
+    def insert(self, word):
+        node = Node(word)
+        node.left = self.nill
+        node.right = self.nill
+        node.color = 1
+
+        # fixing new node's attributes
+        prev = None
+        current = self.root
+
+        while current != self.Nill:
+            prev = current
+            if node.word.lower == current.word.lower:
+                return -1       # Error Indicating word already exists
+            elif node.word.lower < current.word.lower:
+                current = current.left
+            else:
+                current = current.right
+
+        node.parent = prev
+        self.count += 1
+        # fixing parents attributes
+        if prev == None:
+            self.root = node
+        elif node.word.lower < prev.word.lower:
+            prev.left = node
+        else:
+            prev.right = node
+
+        if node.parent == None:
+            node.color = 0
+            return
+
+        if node.parent.parent == None:
+            return
+        # function to fix rbtree due to insert
