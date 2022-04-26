e9c4ba6b087020fe69280fb10bb7ddad23e19e2b
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-04-26 22:31:57 +0200

    Update red_black_tree.py
    adding counter for number of nodes in tree

--- red_black_tree.py
+++ red_black_tree.py
@@ -15,4 +15,5 @@ class RedBlackTree():
         self.nill = Node(0)  # creation of the NILL node
         self.nill.color = 0  # setting the NILL node's color as black
         self.root = self.nill  # setting the RBTree's root as NILL as it is empty
+        self.count = 0
         # some attributes were not added here since they were added in node declaration
