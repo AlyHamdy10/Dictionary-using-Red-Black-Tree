15d3017d90538d734c9a8658ae5a1bfbebb3008c
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-05-02 21:45:46 +0200

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+from red_black_tree import RedBlackTree
+
+
+def load_file(tree, filename):
+    file = open(filename, 'r')
+    for each in file:
+        insert_str(tree, each)
+
+
+def insert_str(tree, str, mode=0):
+    if tree.insert(str) == -1:
+        print("ERROR: Word already in the dictionary")
+    if mode == 1:
+        print("Height = " + str(tree.height(tree.root)))
+        print("Size = " + str(tree.count))
