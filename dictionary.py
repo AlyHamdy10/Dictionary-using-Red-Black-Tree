1271fc803d52446e46a24d04db188361ef380bde
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-05-03 14:28:08 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,30 +4,18 @@ from red_black_tree import RedBlackTree
 def load_file(tree, filename):
     file = open(filename, 'r')
     for each in file:
-        insert_str(tree, each)
+        insert_string(tree, each)
 
 
-def insert_str(tree, str, mode=0):
-    if tree.insert(str) == -1:
+def insert_string(tree, string, mode=0):
+    if tree.insert(string) == -1:
         print("ERROR: Word already in the dictionary")
+        return
     if mode == 1:
         print("Height = " + str(tree.height(tree.root)))
         print("Size = " + str(tree.count))
 
 
-dictionary = RedBlackTree()
-load_file(dictionary, "EN-US-Dictionary.txt")
-# tree.insert('Mariam')
-# tree.insert('Aly')
-# tree.insert('Nayrouz')
-# tree.insert('Zawawy')
-# tree.insert('Magdy')
-# tree.insert('hamdy')
-# tree.insert('ahmed')
 
 
-print(str(dictionary.height(dictionary.root)))
-print(str(dictionary.count))
 
-insert_string(dictionary, 'Aly', 1)
-insert_string(dictionary, 'Aly', 1)
