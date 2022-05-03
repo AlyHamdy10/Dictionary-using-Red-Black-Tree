8e9896f6ea0cb977121e7d65ea086207f4103153
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-05-03 13:41:05 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -13,3 +13,21 @@ def insert_str(tree, str, mode=0):
     if mode == 1:
         print("Height = " + str(tree.height(tree.root)))
         print("Size = " + str(tree.count))
+
+
+dictionary = RedBlackTree()
+load_file(dictionary, "EN-US-Dictionary.txt")
+# tree.insert('Mariam')
+# tree.insert('Aly')
+# tree.insert('Nayrouz')
+# tree.insert('Zawawy')
+# tree.insert('Magdy')
+# tree.insert('hamdy')
+# tree.insert('ahmed')
+
+
+print(str(dictionary.height(dictionary.root)))
+print(str(dictionary.count))
+
+insert_string(dictionary, 'Aly', 1)
+insert_string(dictionary, 'Aly', 1)
